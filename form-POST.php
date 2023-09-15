bed8a8eac62a57aaa91ee4c9f6663e25f547c277
FadimeZgj <fadime.ilhan5703@gmail.com>
2023-09-15 11:52:34 +0200

    style formulaire et success

--- form-POST.php
+++ form-POST.php
@@ -6,8 +6,12 @@ $password = 'root';
 
 $dbh = new PDO($dsn, $user, $password);
 
-if (!empty($_POST['submit'])) {
+if (empty($_GET["res"])) {
+    header("Location: /");
+    die;
+}
 
+if (!empty($_POST['submit'])) {
 
     $errors = [];
 
@@ -56,11 +60,21 @@ if (!empty($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Flop - Merci !</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/form.css">
 </head>
 
 <body>
-    <h1>Let's goooo !</h1>
-    <h2>Votre participation est validée</h2>
+    <div class="container vw-100 vh-100">
+        <div class="row g-3 needs-validation m-auto w-50 bg-light p-5 rounded-5 position-absolute top-50 start-50 translate-middle" action="form-POST.php?res=<?= $_GET['res'] ?>" method="POST" novalidate>
+            <h1>Let's goooo !</h1>
+            <h2>Votre participation est validée !</h2>
+            <div class="m-auto d-flex justify-content-center align-items-center">
+                <img class="w-25" src="assets/images/1200px-Logo_RWC2023_FR.svg.png" alt="">
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 
 </html>
